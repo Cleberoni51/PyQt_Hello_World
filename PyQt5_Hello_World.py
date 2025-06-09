92546491e02ff20f915b05c7f2c4b9408f9375da
Cleberoni51 <caleb.schear@gmail.com>
2025-06-08 21:47:15 -0400

    PyQt5 Hello World opens up a small widget thats titled hello world and says hello world

---
+++ PyQt5_Hello_World.py
@@ -0,0 +1,29 @@
+
+#PyQt 6 Release 1
+#Caleb Schear
+#20250608-CS-PyQt6_Hello_World_1.py
+
+import sys
+import PyQt5.QtWidgets as qtw
+import PyQt5.QtGui as qtg
+
+class MainWindow(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        #Add a Title for your Widget
+        self.setWindowTitle("Hello World!")
+
+        #set Vertical Layout
+        self.setLayout(qtw.QVBoxLayout())
+
+        #create a label
+        my_label = qtw.QLabel("Hello World!")
+        self.layout().addWidget(my_label)
+
+        self.show()
+
+app = qtw.QApplication([])
+mw = MainWindow()
+
+app.exec_()
+
